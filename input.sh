b98bd04cd5935779749a5f9f07dd0f0df0c4ab46
vignesh-waran-k <147132113+vignesh-waran-k@users.noreply.github.com>
2024-02-26 12:44:36 +0000

    Add files via upload

---
+++ input.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Prompt user for project and function names
+PROJECT_NAME="<your-project-name>"
+FUNCTION_NAME="<your-cloud-function-name>"
+LOCATION="us"
+BUCKET_NAME="<your-bucket-name>"
+SERVICE_ACCOUNT_NAME="<your-service_account>"
+SCHEDULER_NAME="<your-scheduler-name>"
